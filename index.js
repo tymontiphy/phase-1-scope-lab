062dbaa1e720b3186c9de97d4d69aa50556a19c7
Your Name <youremail@domain.com>
2024-09-25 16:30:59 +0300

    scope test

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+
+//Global variable
+
+var customerName = "bob"
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+ const leastFavoriteCustomer = "john"
+
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = "another name"
+}
